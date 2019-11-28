64b6733ac283523e1ab0a5b0b14c0fb3236f5952
ilham-zukri <zukri.ilham@gmail.com>
2019-11-28 21:12:48 +0700

    Matriks menggunakan Loop for

--- test.js
+++
@@ -1,8 +0,0 @@
-var i = require('readline-sync'); var a = parseInt(i.question("Masukan Nilai A = ")); var b = parseInt(i.question("Masukan Nilai B ="));
-var c = a * b;
-
-if (c < 100) {
-  console.log(+c+" Lebih Kecil dari 100");
-} else {
-  console.log(+c+" Lebih Besar dari 100");
-}
